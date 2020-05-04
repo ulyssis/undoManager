ec1fdb4e0b3f366bad6d1c5f245250bc8abc1130
ulyssis <li.di.china@gmail.com>
2020-05-04 22:28:52 +0200

    further improvement, basic functions of Undo is done.

--- undo_manager/src/undo/Demo.java
+++ undo_manager/src/undo/Demo.java
@@ -2,17 +2,40 @@ package undo;
 
 public class Demo {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 		
 		
 		SimpleText simpleText = new SimpleText();
 		int bufferSize = 32;
+		EditDescription editDescription = null;
 
 		UndoFactory undoFactory = new UndoFactory();
 		Undo undo = (Undo) undoFactory.createUndoManager(simpleText, bufferSize);
 		
+		System.out.println("-----------test 1:\nafter adding one change:\n");
+
+		// Make a change to the simpleText.
+		editDescription = new EditDescription(TypeOfChange.insert, 0, "hello", 0, 10);
+		
+		undo.canRedo();
+		undo.recordChange(editDescription);
 		undo.canUndo();
+		undo.undo();
+		undo.canUndo();
+		
+		undo.canRedo();
+		undo.redo();
+		
+		// go wrong here.
+		undo.canUndo();
+		
+		// Make a change to the simpleText.
+		editDescription = new EditDescription(TypeOfChange.insert, 0, "hello", 0, 10);
+		
+		// Make a change to the simpleText.
+		editDescription = new EditDescription(TypeOfChange.insert, 0, "hello", 0, 10);
+		
 		
 		
 		
--- undo_manager/src/undo/Undo.java
+++ undo_manager/src/undo/Undo.java
@@ -35,34 +35,27 @@ public class Undo implements UndoManager {
 
 	@Override
 	public boolean canUndo() {
-	if(ringBufferUndo.size() != 0) {
+	if(ringBufferUndo.size() > 0) {
+		System.out.print("Undo can be done, in case you want to make an undo.\n"); 
 		return true;
 	}else {
-		throw new IllegalStateException("change cannot be applied!");
+		System.out.print("Undo can NOT be done, because there is no records of input anymore.\n"); 
+//		throw new IllegalStateException("change cannot be applied!");
+		return false;
 		}
 	}
 	
 	@Override
 	public void undo() {
 		if(canUndo()) {
-			//ByteBufferWithInfo info = ringBufferUndo.dequeue();
-			EditDescription editDescription = new EditDescription(); 
 			try {
-				editDescription = ringBufferUndo.peek();
+				EditDescription editDescription = ringBufferUndo.peek();
 				Edit edit = new Edit(simpleText, editDescription);
 				edit.apply(simpleText);
-				
-				
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			try {
 				ringBufferUndo.remove();
 				ringBufferRedo.add(editDescription);
-				
+				System.out.print("Undo is made!\n"); 
 			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
@@ -72,32 +65,30 @@ public class Undo implements UndoManager {
 	@Override
 	public boolean canRedo() {
 		if(ringBufferRedo.size() != 0) {
+			System.out.print("Redo can be done, in case you want to make a Redo.\n"); 
 			return true;
 		}else {
-			throw new IllegalStateException("change cannot be applied!");
+			System.out.print("Redo can NOT be done right now.\n"); 
+			return false;
+			//			throw new IllegalStateException("change cannot be applied!\n");
 		}
 	}
 
 	@Override
 	public void redo() {
 		if(canRedo()) {
-			//ByteBufferWithInfo info = ringBufferUndo.dequeue();
-			EditDescription editDescription = new EditDescription(); 
-			try {
-				editDescription = ringBufferRedo.peek();
-				Edit edit = new Edit(simpleText, editDescription);
-				edit.revert(simpleText);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			try {
-				ringBufferRedo.remove();
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+				try {
+					EditDescription editDescription = ringBufferRedo.peek();
+					Edit edit = new Edit(simpleText, editDescription);
+					edit.revert(simpleText);
+					ringBufferRedo.remove();
+					ringBufferUndo.add(editDescription);
+					System.out.print("Redo is made!\n"); 
+
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 		}
 	}
-
 }
