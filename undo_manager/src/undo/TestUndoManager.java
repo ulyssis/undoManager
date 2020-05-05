1cab972eb5bc3eebf4c4c0bae8de2bf56d90f732
ulyssis <li.di.china@gmail.com>
2020-05-05 23:15:48 +0200

    change file name

--- undo_manager/src/undo/TestUndoManager.java
+++ undo_manager/src/undo/TestUndoManager.java
@@ -11,7 +11,7 @@ import org.junit.jupiter.api.Test;
  * @author max
  *
  */
-public class UndoTest {
+public class TestUndoManager {
 
 	SimpleText simpleText = new SimpleText();
 	int bufferSize = 5;
